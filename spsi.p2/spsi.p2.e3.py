cad123a5466b02a5059104fba1a66c3190b3eafd
José María Martín Luque <jmmartinluque@gmail.com>
2019-10-19 19:22:06 +0200

    Añadida función afin que cifra y descifra en un criptosistema afín conocidos los parámetros

--- spsi.p2/spsi.p2.e3.py
+++ spsi.p2/spsi.p2.e3.py
@@ -31,3 +31,45 @@ def numeros_a_texto(numeros):
         texto = texto + c
     return texto
 
+def inverso_modulo_n(x, n):
+    """Calcula el inverso de un número 'x' módulo 'n'"""
+    for i in range(n):
+        if ((i*x) % n == 1):
+            return i
+
+def afin(a, b, x, s):
+    """Función que cifra y descifra mediante un criptosistema afín en un 
+    alfabeto de 26 caracteres
+
+    Parámetros:
+    a, b -- claves de cifrado
+    x -- 0 si cifra, 1 si descifra
+    s -- cadena a cifrar
+
+    Cada caracter cifrado 'e' se obtiene mediante la operación
+        e(c) = (a*c + b) % n,
+    donde 'c' es el caracter a cifrar.
+
+    Para descifrar un carácter realizamos la operación
+        c(e) = (a_p*e + b_p) % n,
+    donde 'e' es el carácter cifrado y 'a_p' y 'b_p' vienen dados por
+        a_p = a^-1 % n,
+        b_p = (-a_p * b) % n.
+     
+    Para el cálculo de los inversos módulo 26 se utiliza la función 
+    inverso_modulo_n(x, n)
+    """
+
+    numeros = texto_a_numeros(s)
+
+    if (x):
+        for i, num in enumerate(numeros):
+            a_p = inverso_modulo_n(a, 26)
+            b_p = (-a_p * b) % 26
+            numeros[i] = (a_p*num + b_p) % 26
+    else:
+        for i, num in enumerate(numeros):
+            numeros[i] = (a*num + b) % 26
+    
+    return numeros_a_texto(numeros)
+
